0f43f32ae6362c030e94c8061935c8c17a3bf68c
Daniel Macias <dm2638608@gmail.com>
2025-10-21 10:17:40 -0500

    Login usando Reac,vite Tailwind css

--- turnApp_frontend/cliente-turnApp/src/components/ui/index.js
+++
@@ -1,4 +0,0 @@
-export { Card, CardHeader, CardContent, CardTitle } from './Card';
-export { Button } from './Button';
-export { Input, Label } from './Input';
-export { Layout } from './Layout';
--- turnApp_frontend/cliente-turnApp/src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { LoginPage } from './LoginPage';
-export { AdminDashboard } from './AdminDashboard';
-export { EmployeeDashboard } from './EmployeeDashboard';
