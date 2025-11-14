2b6354e12883ff7949939d4f8c7fd3050abdb723
Neid-09 <neiderqm09@gmail.com>
2025-11-13 21:51:47 -0500

    feat(horarios-microservice): inicializar microservicio de horarios con configuración de Maven y estructura básica

---
+++ turnApp_backend/microservices/horarios-microservice/src/main/java/com/turnapp/microservice/horarios_microservice/HorariosMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.turnapp.microservice.horarios_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorariosMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorariosMicroserviceApplication.class, args);
+	}
+
+}
